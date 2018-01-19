8b0e07899d70f628550b6b007ccdde2134b05cb8
Vasyl Boyko <vasyl.boyko@globallogic.com>
2018-01-19 16:00:05 +0200

    Clean up

--- site_tracker/index.js
+++ site_tracker/index.js
@@ -7,41 +7,24 @@ var sys = require("system"),
 	pass = encodeURIComponent(sys.env.GL_PASS),
     url = 'https://' + user + ':' + pass + '@portal-ua.globallogic.com/officetime/#graph/LWO/701/month/0';
 
-function printArgs() {
-    var i, ilen;
-    for (i = 0, ilen = arguments.length; i < ilen; ++i) {
-        console.log("    arguments[" + i + "] = " + JSON.stringify(arguments[i]));
-    }
-    console.log("");
-}
-
-////////////////////////////////////////////////////////////////////////////////
 page.viewportSize = { width: 600, height: 1000 };
-//the clipRect is the portion of the page you are taking a screenshot of
 page.clipRect = { top: 0, left: 0, width: 750, height: 1000 };
 page.zoomFactor = 0.75;
-page.open(url)
-page.onInitialized = function() {
-    console.log("page.onInitialized");
-    printArgs.apply(this, arguments);
-};
-page.onLoadStarted = function() {
-    console.log("page.onLoadStarted");
-    printArgs.apply(this, arguments);
-};
 page.onLoadFinished = function(status) {
-    console.log("page.onLoadFinished");
-    printArgs.apply(this, arguments);
 	if(status === "success") {
 		setTimeout(function () {
 			page.scrollPosition = {left: 0,	top: 0}
-			console.log("img rendering");
 			page.render('/home/vb/Pictures/desklet/example.png');
-			console.log("img rendered");
-		}, 1000)
+//			console.info("Updated " + (new Date()).toLocaleString());
+		}, 1000);
+	} else {
+		console.error("page.onLoadFinished: faild to load page. Status: " + status);
 	}
 };
 
+console.info("Started " + (new Date()).toLocaleString());
+
+page.open(url)
 setInterval(function () {
 	page.open(url)
 }, 60000)
